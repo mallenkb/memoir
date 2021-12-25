8c923a454b9c140fe5cea5df255e51c3c3ca012f
Marlon Allen <allenmarlon4@gmail.com>
2021-12-25 11:33:32 +0000

    Site Upload
    This is my site

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  darkMode: "media",
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ["Inter", "sans-serif"],
+      },
+
+      colors: {
+        "cod-gray": {
+          DEFAULT: "#121212",
+          50: "#6E6E6E",
+          100: "#646464",
+          200: "#4F4F4F",
+          300: "#3B3B3B",
+          400: "#262626",
+          500: "#121212",
+          600: "#080707",
+          700: "#000000",
+          800: "#000000",
+          900: "#0E0E0E",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
